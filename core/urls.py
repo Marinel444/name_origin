375675940d343260d0d7e9f831e551e3428555c3
Marinel444 <380978907444m@gmail.com>
2025-05-17 19:34:07 +0300

    added 'popular-name' functional, like views and endpoin

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import NameLookupView
+from .views import NameLookupView, PopularNamesView
 
 urlpatterns = [
     path("names/", NameLookupView.as_view(), name="name-lookup"),
+    path("popular-names/", PopularNamesView.as_view(), name="popular-names"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from datetime import timedelta
+
+from django.db.models import Count
 from django.utils import timezone
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -13,7 +15,10 @@ class NameLookupView(APIView):
     def get(self, request):
         name = request.query_params.get("name")
         if not name:
-            return Response({"detail": "Missing 'name' query parameter"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"detail": "Missing 'name' query parameter"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
         recent_threshold = timezone.now() - timedelta(days=1)
         records = Name.objects.filter(name=name, last_accessed_at__gte=recent_threshold)
@@ -24,7 +29,10 @@ class NameLookupView(APIView):
 
         nationalize_data = get_nationalize_data(name)
         if not nationalize_data or not nationalize_data.get("country"):
-            return Response({"detail": f"No country data found for name '{name}'"}, status=404)
+            return Response(
+                {"detail": f"No country data found for name '{name}'"},
+                status=404
+            )
 
         result = []
 
@@ -53,3 +61,35 @@ class NameLookupView(APIView):
 
         serializer = NameSerializer(result, many=True)
         return Response(serializer.data)
+
+
+class PopularNamesView(APIView):
+    def get(self, request):
+        country_code = request.query_params.get("country")
+        if not country_code:
+            return Response(
+                {"detail": "Missing 'country' query parameter"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            country = Country.objects.get(code=country_code)
+        except Country.DoesNotExist:
+            return Response(
+                {
+                    "detail": f"Country with code '{country_code}' not found."
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        top_names = (
+            Name.objects.filter(country=country)
+            .values("name")
+            .annotate(total_requests=Count("request_count"))
+            .order_by("-total_requests")[:5]
+        )
+
+        if not top_names:
+            return Response({"detail": f"No name data found for country '{country_code}'"}, status=404)
+
+        return Response(top_names)
