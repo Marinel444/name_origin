ceffaaa0c04a8008864620fcc7f62e1cc9d90a19
Marinel444 <380978907444m@gmail.com>
2025-05-20 10:31:02 +0300

    added documentation

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
 from .views import NameLookupView, PopularNamesView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Where From API",
+        default_version='v1',
+        description="Guess a person's country of origin by name",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path("names/", NameLookupView.as_view(), name="name-lookup"),
     path("popular-names/", PopularNamesView.as_view(), name="popular-names"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- name_origin/settings.py
+++ name_origin/settings.py
@@ -130,3 +130,15 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
+
+
+SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "Bearer": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header",
+            "description": "JWT Authorization header using the Bearer scheme. Example: 'Bearer <token>'",
+        }
+    },
+}
